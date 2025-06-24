7f167c39e3dd82f436361810ad3ca35faad91aac
jimgar <jimmahgee@gmail.com>
2025-06-24 18:28:59 +0100

    Update main, messy testing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 from textnode import TextType, TextNode
+from htmlnode import LeafNode, ParentNode
 
 
 def main():
     node = TextNode("This is some anchor text", TextType.NORMAL, "blahakljsdff")
     print(node)
+
+    leaf_node = LeafNode("br", "Text to go between the tags")
+    print(leaf_node)
+
+    parent_node = ParentNode(
+        "p",
+        [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ],
+    )
+    print(parent_node.to_html())
 
 
 if __name__ == "__main__":
