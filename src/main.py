3764c827ba1d3c6cc83b9e1c5f6d773cbefcf1fe
jimgar <jimmahgee@gmail.com>
2025-06-25 22:43:49 +0100

    Juat playing around some more

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from textnode import TextType, TextNode
 from htmlnode import LeafNode, ParentNode
+from splitnodes import split_nodes_delimiter
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.NORMAL, "blahakljsdff")
+    node = TextNode("This is some anchor text", TextType.TEXT, "blahakljsdff")
     print(node)
 
     leaf_node = LeafNode("br", "Text to go between the tags")
@@ -19,6 +20,15 @@ def main():
         ],
     )
     print(parent_node.to_html())
+
+    n1 = TextNode("This is text with a `code block` word", TextType.TEXT)
+    n2 = TextNode("This is `code text` with a `code block` word", TextType.TEXT)
+    n3 = TextNode("This is text with a code block word", TextType.TEXT)
+    n4 = TextNode("", TextType.TEXT)
+    n5 = TextNode("Some **donkus**", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([n1, n2, n3, n5, n4], "`", TextType.CODE)
+    for i in new_nodes:
+        print(i)
 
 
 if __name__ == "__main__":
